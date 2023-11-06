0bf280f1769f8ce63092a708351713a53ec5a637
dayvd <allysondayvd@gmail.com>
2023-11-06 01:51:09 -0300

    cadastrar pronto

--- crudphp/cadastrar.php
+++ crudphp/cadastrar.php
@@ -1,38 +1,46 @@
 <?php
 require 'config.php';
 
-// Configurar cabeçalhos CORS para permitir acesso de qualquer origem
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS"); // Adicione outros métodos se necessário
-header("Access-Control-Allow-Headers: Content-Type"); // Adicione outros cabeçalhos se necessário
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    // Lida com solicitações preflight (usadas pelo navegador para verificar permissões)
     header("HTTP/1.1 200 OK");
     exit;
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Lida com a solicitação POST
-    $idSneaker = filter_input(INPUT_POST, 'idSneaker');
+    try {
+        $data = file_get_contents('php://input');
+        $jsonData = json_decode($data);
 
-    $sql = $pdo->prepare("INSERT INTO produtos (idSneaker) VALUES (:idSneaker)");
-    $sql->bindValue(":idSneaker", $idSneaker);
-    $sql->execute();
-    // Verifica se 'idSneaker' não é nulo
-    if ($idSneaker !== null) {
-        // Você pode usar $idSneaker da forma que desejar
-        echo "ID do Sneaker recebido: $idSneaker";
-    } else {
-        echo "nao esta recebendo nenhum valor aqui";
+        if ($jsonData !== null && isset($jsonData->idSneaker)) {
+            $idSneaker = $jsonData->idSneaker;
+
+            $sql = $pdo->prepare("INSERT INTO produtos (idSneaker) VALUES (:idSneaker)");
+            $sql->bindValue(":idSneaker", $idSneaker);
+            $sql->execute();
+
+            if ($sql->rowCount() > 0) {
+                echo "ID do Sneaker inserido com sucesso: $idSneaker";
+            } else {
+                echo "Erro: Não foi possível inserir o ID do Sneaker.";
+            }
+        } else {
+            echo "Erro: Dados inválidos ou ausentes no formato JSON.";
+        }
+    } catch (PDOException $e) {
+        echo "Erro ao inserir o ID do Sneaker: " . $e->getMessage();
     }
 } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    // Lida com a solicitação GET
-    // Você pode adicionar código para responder a solicitações GET aqui
-    // Por exemplo, para buscar dados da tabela produtos e enviar como resposta
-    $sql = $pdo->prepare("SELECT * FROM produtos");
-    $sql->execute();
-    $result = $sql->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($result);
+    try {
+        $sql = $pdo->prepare("SELECT * FROM produtos");
+        $sql->execute();
+        $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($result);
+    } catch (PDOException $e) {
+        echo "Erro ao buscar dados: " . $e->getMessage();
+    }
 }
 ?>
